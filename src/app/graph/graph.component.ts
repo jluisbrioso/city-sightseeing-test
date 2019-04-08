16d68437a335c24b9b238439881b965e6b546a4d
jluisbrioso <jluisbrioso@gmail.com>
2019-04-08 13:07:20 +0200

    Prueba

--- src/app/graph/graph.component.ts
+++ src/app/graph/graph.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../services/data.service';
 
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+
 @Component({
   selector: 'app-graph',
   templateUrl: './graph.component.html',
@@ -11,10 +14,22 @@ export class GraphComponent implements OnInit {
   // Data to display in the chart
   public data: Array<number> = [];
 
+  // Controls subscription cycle
+  private unsub: Subject<any> = new Subject();
+
+
   constructor( private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getGraphValues().subscribe((val) => this.data = [...this.data, val]);
+    this.dataService.getGraphValues()
+    .pipe(takeUntil(this.unsub)) //tells the subscription to stop (when unsub changes)
+    .subscribe((val) => this.data = [...this.data, val]);
   }
+
+  //Lifecycle method to complete subscription
+  ngOnDestroy(): any {
+    this.unsub.next();//changes subject's value
+    this.unsub.complete();
+}
 
 }
--- src/app/scroll/scroll.component.ts
+++ src/app/scroll/scroll.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ICity } from '../services/models';
 import { DataService } from '../services/data.service';
+
 
 @Component({
   selector: 'app-scroll',
@@ -9,12 +10,55 @@ import { DataService } from '../services/data.service';
 })
 export class ScrollComponent implements OnInit {
 
+  //Displayed Array (Viewable)
   public cities: Array<ICity> = [];
+
+  //Cached Array
+  public cachcities: Array<ICity> = [];
+  
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    if ((window.innerHeight + window.scrollY) >= (document.documentElement.scrollHeight - 1)) {
+      this.mobileLoad();
+    }
+  }
+
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.dataService.getCities().subscribe(val => this.cities = val);
+    this.dataService.getCities().subscribe(val => this.mobileAdapt(val))
   }
+
+
+  //Initializes view depending on userAgent.
+  mobileAdapt(cfetch: Array<ICity>): void{
+    if(/mobile/g.test(navigator.userAgent.toLowerCase())){
+      this.cachcities = [...cfetch];
+      this.mobileLoad();
+    } else {
+      this.cities = [...cfetch];
+    }
+  }
+
+
+  //Adds an item to the view a specified number of times.
+  mobileLoad(): void{
+    const loadNumber = 5;
+
+    var times = (this.cachcities.length >= 5 ? loadNumber : this.cachcities.length); // if not enough items, it gets the max available
+
+    for(var i = 0; i < times; i++){
+      this.cacheToView();
+    }
+  }
+
+
+  //Shifts an item from cache to view.
+  cacheToView():void{
+    this.cities = [...this.cities, this.cachcities.shift()];
+  }
+
 
 }
